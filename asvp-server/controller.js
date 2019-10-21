2e17dad2723bb48e4bd65f520aacb7df5037b2d9
RCP <rcpierz@gmail.com>
2019-10-21 17:06:03 -0500

    NOT FUNC. Request now parsed into object

--- asvp-server/controller.js
+++ asvp-server/controller.js
@@ -39,16 +39,210 @@ router.post('/generate', (req, res) =>{
   // Gherkin-Tests.json provides the Gherkin Syntax 
   let gherkinTests = JSON.parse(fs.readFileSync("./gherkin-tests.json"));
 
-  var tests = input.tests;
+  var Config = function() {
+    this.proxyURL = "";
+    this.method = "";
+    this.endpoint;
+    this.parameters = {
+      queryParams: {},
+      headers: {},
+      formParams: {},
+      basicAuth: {},
+      body: ""
+    };
+    this.output = {
+      code: "",
+      headers: {},
+      body: ""
+    };
 
-  // Create the tests using the data in the request
-  for (var test in tests){
+    this.setProxyURL = function(value){
+      this.proxyURL = value;
+    };
+    this.getProxyURL = function(){
+      return this.proxyURL;
+    };
+
+    this.setMethod = function(value){
+      this.method = value;
+    };
+    this.getMethod = function() {
+      return this.method;
+    };
+
+    this.setBasicAuth = function(value){
+      this.parameters.basicAuth = value;
+    };
+    this.getBasicAuth = function() {
+      return this.parameters.basicAuth;
+    };
+
+    this.setEndpoint = function(value){
+      this.endpoint = value;
+    };
+    this.getEndpoint = function(){
+      return this.endpoint;
+    };
+
+    this.setBody = function(value){
+      this.parameters.body = value;
+    };
+    this.getBody = function() {
+      return this.parameters.body;
+    };
+
+
+    this.addQueryParam = function(key, value){
+      this.parameters.queryParams[key] = value;
+    };
+    this.setQueryParams = function(config){
+      for (var queryParam in config){
+        this.addQueryParam(queryParam,config[queryParam]);
+      };
+    };
+    this.getQueryParams = function() {
+      return this.parameters.queryParams;
+    };
+
+
+    this.addHeader = function(key, value){
+      this.parameters.headers[key] = value;
+    };
+    this.getHeaders = function() {
+      return this.parameters.headers;
+    };
+    this.setHeaders = function(config){
+      for (var header in config){
+        this.addheader(header,config[header]);
+      };
+    };
+
+
+    this.addFormParam = function(key, value){
+      this.parameters.formParams[key] = value;
+    };
+    this.getFormParams = function() {
+      return this.parameters.formParams;
+    };
+    this.setFormParams = function(config){
+      for (var formParam in config){
+        this.addFormParam(formParam,config[formParam]);
+      };
+    };
+
+
+    this.setOutputCode = function(value){
+      this.output.code = value;
+    };
+    this.getOutputCode = function() {
+      return this.output.code;
+    };
+
+    this.addOutputHeader = function(key, value){
+      this.output.headers[key] = value;
+    };
+    this.getOutputHeader = function() {
+      return this.output.headers;
+    };
+    this.setOutputHeader = function(value) {
+      this.output.headers = value;
+    }
+
+    this.setOutputBody = function(value) {
+      this.output.body = value;
+    };
+    this.getOutputBody = function() {
+      return this.body;
+    }
+
+
+    this.setConfig = function(config) {
+      this.setProxyURL(config.getProxyURL());
+      this.setMethod(config.getMethod());
+      this.setBasicAuth(config.getBasicAuth());
+      this.setQueryParams(config.getQueryParams());
+      this.setHeaders(config.getHeaders());
+      this.setFormParams(config.getFormParams());
+      this.setBody(config.getBody());
+    };
+
+    this.setMetadata = function(metadata){
+      if (metadata != null){
+        if (metadata.ProxyURL != null){
+          this.setProxyURL(metadata.ProxyURL);
+        };
+        if (metadata.Method != null){
+          this.setMethod(metadata.Method);
+        };
+        if (metadata.Endpoint != null){
+          this.setEndpoint(metadata.Endpoint);
+        };
+      }
+    };
+
+    this.addParameters = function(parameters) {
+      if (parameters != null){
+        if (parameters.BasicAuth != null){
+          this.setBasicAuth(parameters.BasicAuth);
+        };
+        if (parameters.QueryParams != null) {
+          for (var queryParam in parameters.QueryParams){
+            this.addQueryParam(queryParam,parameters.QueryParams[queryParam])
+          };
+        };
+        if (parameters.Headers != null){
+          for (var header in parameters.Headers){
+            this.addHeader(header,parameters.Headers[header]);
+          };
+        };
+        if (parameters.FormParams != null){
+          for(var formParam in parameters.FormParams){
+            this.addFormParam(formParam,parameters.FormParams[formParam]);
+          };
+        };
+        if (parameters.Body != null){
+          this.setBody(parameters.Body);
+        };
+      };
+    };
+
+    this.setExpectedOutput = function(output){
+      if (output != null) {
+        if (output.ResponseCode != null){
+          this.setOutputCode(output.ResponseCode);
+        };
+        if (output.ResponseHeader != null){
+          this.setOutputHeader(output.ResponseHeader);
+        };
+        if (output.ResponseBody != null){
+          this.setOutputBody(output.ResponseBody);
+        };
+      }
+    }
+
+  };
+
+  var globalConfig = new Config();
+  globalConfig.setMetadata(req.global);
+  globalConfig.addParameters(req.global.Parameters);
+  globalConfig.setExpectedOutput(req.global.ExpectedOutput);
+
+  for (var test in req.tests){
+    var currentTest = new Config()
+    currentTest.setConfig(globalConfig);
+    currentTest.setMetadata(req.tests[test]);
+    currentTest.addParameters(req.tests[test].Parameters);
+    currentTest.setExpectedOutput(req.tests[test].ExpectedOutput);
+    console.log(currentTest);
+
+
+
     var first = true;
     outputTests = outputTests.concat("Scenario: Run Test ",test,"\n");
 
-    // Append all requirements for the request as GIVEN statements
+    // Append all requirements for the req as GIVEN statements
     // Begin GIVEN lines -v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v
-    var parameters = tests[test].Parameters;
+    var parameters = currentTest.parameters;
     for (var parameterType in parameters){
       
       // Write the GIVEN lines for each test
@@ -56,46 +250,46 @@ router.post('/generate', (req, res) =>{
       else outputTests = outputTests.concat("And ");
         
       // Write Query Parameters to Given lines
-      if (parameterType == "QueryParams") {
+      if (parameterType == "queryParams") {
         outputTests = outputTests.concat(gherkinTests.Given.QueryParamStart);         // Append the intro line for Query Parameters
-        for (var queryParam in tests[test].Parameters.QueryParams) {
+        for (var queryParam in currentTest.parameters.queryParams) {
           outputTests = outputTests.concat(gherkinTests.Given.QueryParamUnit
             .replace(/<QUERY_KEY>/,queryParam)
-            .replace(/<QUERY_VALUE>/,tests[test].Parameters.QueryParams[queryParam]));
+            .replace(/<QUERY_VALUE>/,currentTest.parameters.queryParams[queryParam]));
         }
       }
 
       // Write Headers to Given lines
-      else if (parameterType == "Headers") {
+      else if (parameterType == "headers") {
         outputTests = outputTests.concat(gherkinTests.Given.HeaderStart);         // Append the intro line for Headers
-        for (var header in tests[test].Parameters.Headers) {
+        for (var header in currentTest.parameters.headers) {
           outputTests = outputTests.concat(gherkinTests.Given.HeaderUnit
             .replace(/<HEADER_KEY>/,header)
-            .replace(/<HEADER_VALUE>/,tests[test].Parameters.Headers[header]));
+            .replace(/<HEADER_VALUE>/,currentTest.parameters.headers[header]));
         }
       }
 
       // Write Form Parameters to Given lines
-      else if (parameterType == "FormParams") {
+      else if (parameterType == "formParams") {
         outputTests = outputTests.concat(gherkinTests.Given.FormParamStart);         // Append the intro line for Form Parameters
-        for (var formParam in tests[test].Parameters.FormParams) {
+        for (var formParam in currentTest.parameters.formParams) {
           outputTests = outputTests.concat(gherkinTests.Given.FormParamUnit
             .replace(/<FORM_KEY>/,formParam)
-            .replace(/<FORM_VALUE>/,tests[test].Parameters.FormParams[formParam]));
+            .replace(/<FORM_VALUE>/,currentTest.parameters.formParams[formParam]));
         }
       }
 
       // Write Basic Authentication to Given lines
-      else if (parameterType == "BasicAuth") {
+      else if (parameterType == "basicAuth") {
         outputTests = outputTests.concat(gherkinTests.Given.BasicAuth
-          .replace(/<BASIC_USR>/,tests[test].Parameters.BasicAuth.username)
-          .replace(/<BASIC_PSW>/,tests[test].Parameters.BasicAuth.password));
+          .replace(/<BASIC_USR>/,currentTest.parameters.basicAuth.username)
+          .replace(/<BASIC_PSW>/,currentTest.parameters.basicAuth.password));
       }
 
       // Write the Payload to Given lines
-      else if (parameterType == "Body") {
+      else if (parameterType == "body") {
         outputTests = outputTests.concat(gherkinTests.Given.Body.
-          replace(/<BODY>/,tests[test].Parameters.Body));
+          replace(/<BODY>/,currentTest.parameters.body));
       }
     
       
@@ -104,47 +298,48 @@ router.post('/generate', (req, res) =>{
     
 
     // Begin WHEN lines -v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v
-    outputTests = outputTests.concat("When ",gherkinTests.When.RequestEndpoint
-      .replace(/<REQUEST_METHOD>/,tests[test].Method)
-      .replace(/<REQUEST_URL>/,tests[test].Endpoint));
+    outputTests = outputTests.concat("When ",gherkinTests.When.reqEndpoint
+      .replace(/<req_METHOD>/,currentTest.method)
+      .replace(/<req_URL>/,currentTest.endpoint));
     // End WHEN lines -^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
 
 
     // Begin THEN lines -v-v-v--v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v-v
     first = true;
-    var expectedOutput = tests[test].ExpectedOutput
+    var expectedOutput = currentTest.output;
     for (var expectedResponseType in expectedOutput){
       if (first) outputTests = outputTests.concat("Then ");
       else outputTests = outputTests.concat("And ");
 
-      if (expectedResponseType == "ResponseCode"){
+      if (expectedResponseType == "code"){
         outputTests = outputTests.concat(gherkinTests.Then.ResponseCode
-          .replace(/<RESPONSE_CODE>/,tests[test].ExpectedOutput.ResponseCode));
+          .replace(/<RESPONSE_CODE>/,expectedOutput.code));
       }
 
-      else if (expectedResponseType == "ResponseHeader"){
-
-        for (var header in tests[test].ExpectedOutput.ResponseHeader){
+      else if (expectedResponseType == "headers"){
+        for (var header in expectedOutput.headers){
           outputTests = outputTests.concat(gherkinTests.Then.ResponseHeader
             .replace(/<HEADER_KEY>/,header)
-            .replace(/<HEADER_VALUE>/,tests[test].ExpectedOutput.ResponseHeader[header]));
+            .replace(/<HEADER_VALUE>/,expectedOutput.headers[header]));
         }
       }
 
-      else if (expectedResponseType == "ResponseBody") {
-        for (var body in tests[test].ExpectedOutput.ResponseBody){
+      else if (expectedResponseType == "body") {
+        for (var bodyKey in expectedOutput.body){
           outputTests = outputTests.concat(gherkinTests.Then.ResponseBody
-            .replace(/<BODY_PATH>/,body)
-            .replace(/<BODY_VALUE>/,tests[test].ExpectedOutput.ResponseBody[body]));
+            .replace(/<BODY_PATH>/,bodyKey)
+            .replace(/<BODY_VALUE>/,expectedOutput.body[bodyKey]));
       
         }
       }
       first = false;
     }
 
-    // End THEN lines -^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
-    
-  } 
+  // End THEN lines -^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
+  
+  }
+  console.log(outputTests);
+ 
 
   // Generate the feature file from the parameters desired by the user
   //
@@ -156,7 +351,7 @@ router.post('/generate', (req, res) =>{
   });
 
 
-  // Change the placeholder variables in init.js with data from the request
+  // Change the placeholder variables in init.js with data from the req
   //
   console.log('Writing --> Proxy URL @ ./features/support/init.js');
   fs.readFile('./default/default_init.js', 'utf8', function(err, file) {
@@ -165,7 +360,7 @@ router.post('/generate', (req, res) =>{
     };
 
     let inputData = file
-      .replace(/INPUT_URL/g,'\''+input.global.ProxyURL+'\'')
+      .replace(/INPUT_URL/g,'\''+req.global.ProxyURL+'\'')
     fs.writeFileSync('./features/support/init.js', inputData, function(err){
       if (err){
         outputTests = outputTests.concat(err);
