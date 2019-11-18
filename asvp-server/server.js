e16f75ed110e286b971278670f950c59ec022176
Robert Pierz <rpierz@prokarma.com>
2019-11-18 14:31:09 -0600

    Added HTTPS to server

--- asvp-server/server.js
+++ asvp-server/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const https = require('https');
 const controller = require('./controller.js');
 const favicon = require('serve-favicon');
+const fs = require('fs');
 
 const app = express();
+
+var asvpkey = fs.readFileSync('server.key');
+var asvpcert = fs.readFileSync('server.crt');
+
+var options = {
+  key: asvpkey,
+  cert: asvpcert
+}
 
 app.use(morgan(':date[web]'));
 app.use(morgan('dev'));
@@ -23,8 +33,11 @@ app.use( (req, res, next) => {
 
 app.use('/', controller);
 
-app.listen(8000);
+var server = https.createServer(options, app);
+
+server.listen(8000, () => {
 console.log('Listening on 8000...');
+});
 
 process.on('SIGINT', function() {
   process.exit();
